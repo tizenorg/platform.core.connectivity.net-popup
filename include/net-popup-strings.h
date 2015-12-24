442929201a44116af918bf9d3ed58a4979eacda2
Seonah Moon <seonah1.moon@samsung.com>
2015-12-24 09:58:10 +0900

    Initialize and remove Ecore_X dependency for wayland
    Change-Id: I4b90c57c49aa0487738c502c8bc0254898261795
    Signed-off-by: Seonah Moon <seonah1.moon@samsung.com>

--- include/net-popup-strings.h
+++ include/net-popup-strings.h
@@ -1,13 +1,13 @@
 /*
 *  net-popup
 *
-* Copyright 2012-2013  Samsung Electronics Co., Ltd
+* Copyright 2012  Samsung Electronics Co., Ltd
 *
-* Licensed under the Apache License, Version 2.0 (the "License");
+* Licensed under the Flora License, Version 1.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-* http://www.apache.org/licenses/LICENSE-2.0
+* http://www.tizenopensource.org/license
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -24,11 +24,20 @@
 
 #include "net-popup.h"
 
-#define ALERT_STR_MOBILE_NETWORKS_CHARGE		dgettext(PACKAGE, "IDS_ST_POP_CONNECTING_TO_MOBILE_NETWORKS_ING_N_ADDITIONAL_CHARGES_MAY_BE_INCURRED_IF_YOU_ARE_NOT_USING_A_FLAT_RATE_DATA_PLAN")
-#define ALERT_STR_ERR_UNAVAILABLE		dgettext(PACKAGE, "IDS_IM_POP_SERVICE_NOT_AVAIL")
-#define ALERT_STR_ERR_CONNECT			dgettext(PACKAGE, "IDS_ST_BODY_UNABLE_TO_CONNECT_TO_THE_NETWORK")
-#define ALERT_STR_ERR_NOT_SUPPORT		dgettext(PACKAGE, "IDS_ST_POP_NOT_SUPPORTED")
-#define ALERT_STR_WIFI_CONNECTED		dgettext(PACKAGE, "IDS_WIFI_BODY_CONNECTED")
-#define ALERT_STR_RESTRICTED_USE_WIFI	dgettext(PACKAGE, "IDS_COM_POP_SECURITY_POLICY_RESTRICTS_USE_OF_WI_FI")
+#define ALERT_STR_MOBILE_NETWORKS_CHARGE		dgettext(PACKAGE, "IDS_QP_POP_YOU_WILL_BE_CONNECTED_TO_YOUR_MOBILE_NETWORK_THIS_MAY_RESULT_IN_ADDITIONAL_CHARGES")
+#define ALERT_STR_ERR_UNAVAILABLE			dgettext(PACKAGE, "IDS_IM_POP_SERVICE_NOT_AVAIL")
+#define ALERT_STR_ERR_CONNECT				dgettext(PACKAGE, "IDS_WIFI_POP_UNABLE_TO_CONNECT")
+#define ALERT_STR_ERR_WRONG_PASSWORD		dgettext(PACKAGE, "IDS_ST_POP_WRONG_PASSWORD")
+#define ALERT_STR_ERR_NOT_SUPPORT			dgettext(PACKAGE, "IDS_ST_POP_NOT_SUPPORTED")
+#define ALERT_STR_WIFI_CONNECTED			dgettext(PACKAGE, _("IDS_ST_BODY_CONNECTED_TO_WI_FI_NETWORK_PS"))
+#define ALERT_STR_RESTRICTED_USE_WIFI		dgettext(PACKAGE, "IDS_COM_POP_SECURITY_POLICY_RESTRICTS_USE_OF_WI_FI")
+#define ALERT_STR_NO_AP_FOUND			dgettext(PACKAGE, "IDS_WIFI_POP_NO_WI_FI_AP_FOUND")
+#define ALERT_STR_WIFI_MOBILE_AP_ON			dgettext(PACKAGE, "IDS_ST_POP_TURNING_ON_WI_FI_WILL_DISABLE_WI_FI_TETHERING")
+#define ALERT_STR_LENGHTY_PASSWORD			dgettext(PACKAGE, "IDS_COM_POP_MAXIMUM_NUMBER_OF_CHARACTERS_REACHED")
+#define ALERT_STR_DONT_ASK_AGAIN			dgettext(PACKAGE, "IDS_WIFI_BUTTON_DONT_ASK_AGAIN")
+#define ALERT_STR_OK						dgettext(PACKAGE, "IDS_COM_SK_OK")
+#define ALERT_STR_NO						dgettext(PACKAGE, "IDS_COM_SK_NO")
+#define ALERT_STR_CANCEL						dgettext(PACKAGE, "IDS_COM_SK_CANCEL")
+#define ALERT_STR_WIFI 							dgettext(PACKAGE, "IDS_WIFI_BODY_WI_FI")
 
 #endif /* __DEF_NET_POPUP_STRINGS_H__ */
